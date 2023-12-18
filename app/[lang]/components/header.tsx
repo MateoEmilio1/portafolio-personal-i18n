1d0a14bb046c47ca9a49782ba61151c018dd2958
Tute <mateoyastor60@gmail.com>
2023-12-18 06:08:59 -0300

    botton curriculum

--- app/[lang]/components/header.tsx
+++ app/[lang]/components/header.tsx
@@ -3,6 +3,8 @@ import CustomLink from './custom-link'
 
 import { Locale } from '@/i18n.config'
 import { getDictionary } from '@/lib/dictionary'
+import { Button } from '../components/ui/button'
+import Link from 'next/link'
 
 export default async function Header({ lang }: { lang: Locale }) {
   const { navigation } = await getDictionary(lang)
@@ -10,7 +12,8 @@ export default async function Header({ lang }: { lang: Locale }) {
   return (
     <header className='py-6'>
       <nav className='container flex items-center justify-between'>
-        <ul className='flex gap-x-8'>
+
+        {/* <ul className='flex gap-x-8'>
           <li>
             <CustomLink href='/' lang={lang}>
               {navigation.home}
@@ -21,9 +24,24 @@ export default async function Header({ lang }: { lang: Locale }) {
               {navigation.about}
             </CustomLink>
           </li>
-        </ul>
-        
+        </ul> */}
+
+        <a
+          className='ml-2 rounded-md bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 text-white'
+          href='https://drive.google.com/drive/folders/1mVeYA3crYMvmJnp3LlYqR3O9F-67vzk_?usp=drive_link'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          {' '}
+          Curriculum
+        </a>
+
+
+        <div>
+
         <LocaleSwitcher />
+
+        </div>
       </nav>
     </header>
   )
