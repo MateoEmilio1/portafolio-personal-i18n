061744c054fd51cc795665f4d7700c181de857c5
Tute <mateoyastor60@gmail.com>
2023-12-20 12:15:34 -0300

    added Accordion

--- app/[lang]/pages/nosotros/page.tsx
+++ app/[lang]/pages/nosotros/page.tsx
@@ -3,6 +3,12 @@ import logoL2 from '../../../../public/images/hero/logoL2.svg'
 import styles from './hero.module.css'
 import { Locale } from '@/i18n.config'
 import { getDictionary } from '@/lib/dictionary'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger
+} from '../../components/ui/accordion'
 
 export default async function Nosotros({ lang }: { lang: Locale }) {
   const { page } = await getDictionary(lang)
@@ -13,34 +19,57 @@ export default async function Nosotros({ lang }: { lang: Locale }) {
       className=" flex  min-h-screen min-w-full items-center bg-[url('../../public/images/hero/background.png')] bg-cover  bg-center bg-no-repeat   "
     >
       <div className='flex min-h-screen min-w-full items-center bg-black bg-opacity-40 bg-cover bg-center bg-no-repeat'>
-        <div className='px-auto mx-[40px] sm:mt-14  my-6 h-[700px]  min-w-[300px] items-center rounded-lg border-[2px] bg-white bg-opacity-10  pt-6 sm:pt-[40px]   '>
-          <h1 className='px-auto font-inter mx-[24px] sm:pb-10 text-left text-4xl font-bold leading-[58px] tracking-[-0.05em] text-white md:text-5xl lg:text-[48px] xl:text-[48px]'>
+        <div className='px-auto mx-[40px] my-6  h-[700px] min-w-[300px]  items-center rounded-lg border-[2px] bg-white bg-opacity-10 pt-6  sm:mt-14 sm:pt-[40px]   '>
+          <h1 className='px-auto font-inter mx-[24px] text-left text-4xl font-bold leading-[58px] tracking-[-0.05em] text-white sm:pb-10 md:text-5xl lg:text-[48px] xl:text-[48px]'>
             {page.home.nosotros}
           </h1>
 
           <p className='px-auto mx-[24px] font-sans font-normal italic leading-normal tracking-tighter text-white sm:text-lg md:text-xl lg:text-2xl xl:text-2xl'>
             {page.home.nosotros_desc}
           </p>
-          {/* VISION */}
-          <div className='px-auto max-h-auto mx-[24px] mt-9 sm:mt-14 min-h-[127px] items-center rounded-lg border-[2px] bg-white bg-opacity-10 pb-4 px-[24px]'>
-            <h1 className='font-inter sm:pb-3 sm:pt-3 text-left text-3xl sm:text-4xl font-bold leading-[58px] tracking-[-0.05em] text-white md:text-5xl lg:text-[48px] xl:text-[48px]'>
-              {page.home.vision}
-            </h1>
 
-            <p className='font-sans font-normal italic leading-normal tracking-tighter text-white sm:text-lg md:text-xl lg:text-2xl xl:text-2xl'>
-              {page.home.vision_desc}
-            </p>
+          {/* VISION */}
+          <div className='px-auto max-h-auto mx-[24px] mt-9 min-h-[127px] items-center rounded-lg border-[2px] bg-white bg-opacity-10 px-[24px] pb-4 sm:mt-14'>
+            {/* VISION Accordion */}
+
+            <Accordion className='text-white' type='single' collapsible>
+              <AccordionItem value='item-1'>
+                <AccordionTrigger>
+                  <h1 className='font-inter text-left text-3xl font-bold leading-[58px] tracking-[-0.05em] text-white sm:pb-3 sm:pt-3 sm:text-4xl md:text-5xl lg:text-[48px] xl:text-[48px]'>
+                    {page.home.vision}
+                    <p className='mt-3 font-sans font-normal italic leading-normal tracking-tighter text-white sm:text-lg md:text-xl lg:text-2xl xl:text-2xl'>
+                      {page.home.vision_desc}
+                    </p>
+                  </h1>
+                </AccordionTrigger>
+                <AccordionContent>
+                  <p className='font-sans font-normal italic leading-normal tracking-tighter text-white sm:text-lg md:text-xl lg:text-2xl xl:text-2xl'>
+                    {page.home.vision_desc_accordion}
+                  </p>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           </div>
 
           {/* MISION */}
-          <div className='px-auto max-h-auto mx-[24px] mt-9 sm:mt-14 min-h-[127px] items-center rounded-lg border-[2px] bg-white bg-opacity-10 pb-4 px-[24px]'>
-            <h1 className='font-inter sm:pb-3 sm:pt-3 text-left text-3xl sm:text-4xl font-bold leading-[58px] tracking-[-0.05em] text-white md:text-5xl lg:text-[48px] xl:text-[48px]'>
-              {page.home.mision}
-            </h1>
-
-            <p className='font-sans font-normal italic leading-normal tracking-tighter text-white sm:text-lg md:text-xl lg:text-2xl xl:text-2xl'>
-              {page.home.mision_desc}
-            </p>
+          <div className='px-auto max-h-auto mx-[24px] mt-9 min-h-[127px] items-center rounded-lg border-[2px] bg-white bg-opacity-10 px-[24px] pb-4 sm:mt-14'>
+            <Accordion className='text-white' type='single' collapsible>
+              <AccordionItem value='item-1'>
+                <AccordionTrigger>
+                  <h1 className='font-inter text-left text-3xl font-bold leading-[58px] tracking-[-0.05em] text-white sm:pb-3 sm:pt-3 sm:text-4xl md:text-5xl lg:text-[48px] xl:text-[48px]'>
+                    {page.home.mision}
+                    <p className='font-sans font-normal italic leading-normal tracking-tighter text-white sm:text-lg md:text-xl lg:text-2xl xl:text-2xl'>
+                      {page.home.mision_desc}
+                    </p>
+                  </h1>
+                </AccordionTrigger>
+                <AccordionContent>
+                  <p className='font-sans font-normal italic leading-normal tracking-tighter text-white sm:text-lg md:text-xl lg:text-2xl xl:text-2xl'>
+                    {page.home.mision_desc_accordion}
+                  </p>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,20 @@ module.exports = {
       },
     },
     extend: {
-      backgroundImage: theme => ({
-        'custom-gradient': "linear-gradient(142.71deg, #287AD6 -8.47%, #9DBBF5 46.74%, #003F86 109.3%), linear-gradient(0deg, #464646, #464646)",
-      }),
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
